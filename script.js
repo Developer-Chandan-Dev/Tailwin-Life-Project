8fb16911bc76297e2d5d81c5b4c01256f7458736
Developer-Chandan-Dev <chandan.dev.pro@gmail.com>
2024-09-09 06:47:54 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// let Input = document.getElementsByClassName("input");
+// let userName = document.getElementsByClassName("user-name");
+
+// function Show() {
+//     for (UserName of userName) {
+//         UserName.classList.remove("active-user");
+//     }
+//     for (inPut of Input) {
+//         inPut.classList.remove("active-input");
+//     }
+//     let i = Array.from(userName).indexOf(event.target);
+
+//     userName[i].classList.add("active-user");
+//     Input[i].classList.add("active-input");
+// }
+
+const myarr = ["chandan", 48, 37, "Ajay", "shiva", "kundan"];
+console.log(myarr);
